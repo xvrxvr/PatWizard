1f11323c1d5492b7e73fb4436926fb23c6951ca1
unknown <rakhvato@rakhvato-MOBL2.ccr.corp.intel.com>
2015-10-07 17:40:23 +0300

    Initial commit

---
+++ PatWizard.pro
@@ -0,0 +1,21 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-10-07T15:41:35
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = PatWizard
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h \
+    gr_object.h
+
+FORMS    += mainwindow.ui
---
+++ gr_object.h
@@ -0,0 +1,43 @@
+#ifndef GR_OBJECT_H
+#define GR_OBJECT_H
+
+struct GrShape {
+    enum Type {
+        MoveTo,
+        LineTo,
+        ArcTo,
+        ArcCenter,
+        ArcCenter2, // For ellipse
+        ClosePath,
+        Text
+    } type;
+    uint32_t x,y;
+    uint32_t options;
+    const char* text;
+
+    enum Options {
+        FixedPoint     = 0x00000001,
+        ArcCW          = 0x00000002,
+        FillPath       = 0x00000004,
+        ArcIsEllips    = 0x00000008
+    };
+};
+
+class GrObject {
+public:
+    virtual QVector<GrShape> get_image() =0;
+    virtual QMap<QString,uint32_t> get_constrain_vars() =0;
+    virtual QVector<Constrain*> get_constrains() =0;
+};
+
+class GrReader {
+public:
+    virtual void destroy() =0;
+    virtual void read(QMap<QString,QString> config) =0;
+    virtual QVector<GrObject*> get_objects() =0;
+
+    virtual void update() =0;
+};
+
+#endif // GR_OBJECT_H
+
