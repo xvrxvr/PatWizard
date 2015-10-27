816ac3de522b17ec6cacd2b829665e9264bf9d90
Timur Mullayanov <TimurM@bitbucket.org>
2015-10-27 22:30:20 +0300

    [gfx][!] added menu bar, example of creation menu entry and connecting it to the slot

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,6 +7,21 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     ui->statusBar->showMessage("Initialized!");
+
+    //NOTE: when there will be more menu entries (and more actions),
+    // it is recommended to refactor the code below
+    // and extract menu creation to another method like createMenus().Now it's left "as is" to show the flow.
+    fileMenu = ui->menuBar->addMenu(tr("&File"));
+
+    openFileAction = new QAction(tr("&Open"), this);
+    openFileAction->setShortcuts(QKeySequence::Open);
+    openFileAction->setStatusTip(tr("Open PCB file"));
+    connect(openFileAction, SIGNAL(triggered()), this, SLOT(openFile()));
+    fileMenu->addAction(openFileAction);
+}
+
+void MainWindow::openFile() {
+    ui->statusBar->showMessage("openFile() not implemented yet!");
 }
 
 MainWindow::~MainWindow()
