f9346ce70b88627582fcbddcad7c860ef06acbb9
Timur Mullayanov <TimurM@bitbucket.org>
2015-12-01 22:15:51 +0300

    moved local include to cpp, used in forward-decl in header

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 
 #include <QtWidgets>
+#include "wizardscene.h"
 
 MainWindow::MainWindow() {
     createActions();
--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <wizardscene.h>
+
+class WizardScene;
 
 QT_BEGIN_NAMESPACE
 class QAction;
