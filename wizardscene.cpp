aee77651893f281632ac391e15897b594d413a48
Timur Mullayanov <TimurM@bitbucket.org>
2015-12-01 23:18:42 +0300

    added small rect for testing

--- wizardscene.cpp
+++ wizardscene.cpp
@@ -5,6 +5,7 @@ WizardScene::WizardScene(QMenu* itemMenu_, QObject* parent)
 {
     itemMenu = itemMenu_;
     lineColor = Qt::black;
+    addRect(300, 200, 100, 200);
 }
 
 void WizardScene::setLineColor(const QColor &color)
