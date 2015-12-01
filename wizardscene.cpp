77fdfb369b5b29a36075ac0b84c74a1c04efa037
Timur Mullayanov <TimurM@bitbucket.org>
2015-12-01 20:37:25 +0300

    [gfx_gview] real commit with introducing WizardScene class. Functionality's still limited

--- wizardscene.cpp
+++ wizardscene.cpp
@@ -1,7 +1,14 @@
 #include "wizardscene.h"
 
-WizardScene::WizardScene()
+WizardScene::WizardScene(QMenu* itemMenu, QObject* parent)
+    : QGraphicsScene(parent)
 {
+    mItemMenu  = itemMenu;
+    mLineColor = Qt::black;
+}
 
+void WizardScene::setLineColor(const QColor &color)
+{
+    mLineColor = color;
 }
 
--- wizardscene.h
+++ wizardscene.h
@@ -1,11 +1,29 @@
 #ifndef WIZARDSCENE_H
 #define WIZARDSCENE_H
 
+#include <QGraphicsScene>
+
+QT_BEGIN_NAMESPACE
+class QMenu;
+class QColor;
+class QFont;
+class QPointF;
+QT_END_NAMESPACE
+
 
 class WizardScene : public QGraphicsScene
 {
+    Q_OBJECT
+
 public:
-    WizardScene();
+    explicit WizardScene(QMenu* itemMenu, QObject* parent=0);
+    QColor lineColor() const { return mLineColor;}
+    void setLineColor(const QColor& color);
+
+private:
+    QMenu* mItemMenu;
+    QColor mLineColor;
+
 };
 
 #endif // WIZARDSCENE_H
